daf713f227d4d7752b79eaca74a11641bbd776e6
phaxdigit <37367211+phaxdigit@users.noreply.github.com>
2018-06-27 09:49:25 +0100

    Add files via upload

---
+++ project-pixel-art-maker-starter-master/project-pixel-art-maker-starter-master/designs.js
@@ -0,0 +1,35 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+var height, width, color;
+// Your code goes here!
+//$('tr').empty();
+ $('#sizePicker').submit(function (event) {
+    event.preventDefault();
+    height = $('#inputHeight').val();
+    width = $('#inputWeight').val();
+    makeGrid(height, width);
+})
+
+function makeGrid(x, y) {
+
+  // Your code goes here!
+  for (var i = 1; i <= x; i++) {
+      $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+      for (var j = 1; j <=y; j++) {
+          $('#table' + i).append('<td></td>');
+        }
+    }
+
+    // add colors to the cells
+   $('td').click(function addColor(){
+        color = $('#colorPicker').val();
+
+        if ($(this).attr('style')) {
+            $(this).removeAttr('style')
+        } else {
+            $(this).attr('style', 'background-color:' + color);
+        }
+    })
+}
